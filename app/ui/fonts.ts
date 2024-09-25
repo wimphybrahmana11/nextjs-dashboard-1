254f195eb77fa73f458f70858796279be7c55e1e
wimphybrahmana11 <wimphybrahmana@gmail.com>
2024-09-25 12:00:54 +0700

    INIT

---
+++ app/ui/fonts.ts
@@ -0,0 +1,9 @@
+import { Inter } from 'next/font/google';
+import { Lusitana } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Lusitana({
+  subsets: ['latin'], // Tentukan subset yang kamu butuhkan, misalnya 'latin'
+  weight: ['400', '700'], // Atur font weight, bisa 400 (normal) dan 700 (bold)
+});// ...
+ 
